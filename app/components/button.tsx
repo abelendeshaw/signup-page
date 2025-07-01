5493782060500ed752858932db953164d58291b2
abelendeshaw <contact.abelendeshaw@gmail.com>
2025-07-01 12:18:09 +0300

    changed button color

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -14,7 +14,7 @@ export default function Button({
     <button
       type="button"
       onClick={onClick}
-      className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-red-700 transition duration-200"
+      className="w-full bg-red-600 text-white py-3 rounded-lg font-semibold hover:bg-red-700 transition duration-200"
     >
       {children}
     </button>
